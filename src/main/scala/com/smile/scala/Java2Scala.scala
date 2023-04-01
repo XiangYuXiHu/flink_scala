dfb458b4ce23351ff9b143a08e65d9e84273b477
XiangYuXiHu <1278043194@qq.com>
2023-04-01 12:07:08 +0800

    java 2 scala

---
+++ src/main/scala/com/smile/scala/Java2Scala.scala
@@ -0,0 +1,23 @@
+package com.smile.scala
+
+import scala.collection.{JavaConverters, mutable}
+import scala.collection.mutable.ArrayBuffer
+import java.util
+
+object Java2Scala {
+
+  def main(args: Array[String]): Unit = {
+    val element = ArrayBuffer("hello", "world", "china")
+
+    /**
+     * scala to java
+     */
+    val list: util.List[String] = JavaConverters.bufferAsJavaListConverter(element).asJava
+    println(list)
+    val scalaBuffer: mutable.Buffer[String] = JavaConverters.asScalaBufferConverter(list).asScala
+    for (elem <- scalaBuffer) {
+      println(elem)
+    }
+  }
+
+}
